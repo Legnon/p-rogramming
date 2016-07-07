62f35f92c4ab90aee8c29e73ec504265ea8d37e9
KongHyeontaek <rotanev7@gmail.com>
2016-07-07 19:09:15 +0900

    16/7/7 수업내용

--- programming/settings.py
+++ programming/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bx^r(53y-rp=&z4&b7_44jd60+)g^=(pq)$oacg3qw_-rh*$_4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost',]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE_CLASSES = [
