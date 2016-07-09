909a122ffb3beafb483a5b2db8fa59408336c1d3
KongHyeontaek <rotanev7@gmail.com>
2016-07-09 19:56:44 +0900

    add list and detail view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from blog.models import Post
 
 # Create your views here.
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+def introduce(request):
+    return render(request, 'blog/introduce.html', {})
 
+post_list = ListView.as_view(model=Post)
+post_detail = DetailView.as_view(model=Post)
