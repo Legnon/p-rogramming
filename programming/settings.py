b0eff80b3b34aadd9925698b55cb8524b15aa200
KongHyeontaek <rotanev7@gmail.com>
2016-07-22 16:35:18 +0900

    템플릿 상속

--- programming/settings.py
+++ programming/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'programming.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'programming', 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
